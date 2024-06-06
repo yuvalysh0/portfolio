77f408435c61953fd150b01b5eb3d83988794e2b
Yuval Shalom <yuvalysh0@gmail.com>
2024-06-06 12:20:19 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <main>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Hello World</h1>
+    </div>
+   </main>
+  );
+}
