af706e17749d8fb63636415d73edb57f4d41a7d0
Yuval Shalom <yuvalysh0@gmail.com>
2024-06-06 14:21:44 +0300

    added utils

---
+++ app/utils/heroText.ts
@@ -0,0 +1,6 @@
+export const heroText = {
+  title: "Yuval Shalom.",
+  subtitle: "Passionate Frontend Developer",
+  description: "Frontend Developer who transforms ideas into stunning web applications by day and scores goals on the football field by night.",
+};
+
