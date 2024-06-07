715a3ccbc2c87387ceef5880c9dac45c83dc834a
Yuval Shalom <yuvalysh0@gmail.com>
2024-06-07 17:21:57 +0300

    added react-scroll for the navigation

--- app/components/layout/NavBar.tsx
+++ app/components/layout/NavBar.tsx
@@ -3,17 +3,19 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { playfair } from '../../utils/fonts';
+import { Link as ScrollLink } from 'react-scroll';
 
 const links = [
-  { href: '/', label: 'Home' },
-  { href: '#about', label: 'About' },
-  { href: '#experience', label: 'Experience' },
-  { href: '/projects', label: 'Projects' },
-  { href: '/contact', label: 'Contact' },
+  { href: 'home', label: 'Home' },
+  { href: 'about', label: 'About' },
+  { href: 'experience', label: 'Experience' },
+  { href: 'projects', label: 'Projects' },
+  { href: 'contact', label: 'Contact' },
 ];
 
 const NavBar = () => {
-  const [isSelected, setIsSelected] = useState('');
+  const [isSelected, setIsSelected] = useState('Home');
+
   return (
     <nav className="sticky navbar top-0 w-screen md:w-full flex justify-between items-center py-4 px-8 bg-base-100 z-50">
       <Link href="/" className={playfair.className}>
@@ -21,16 +23,20 @@ const NavBar = () => {
       </Link>
       <div className="hidden md:flex gap-4">
         {links.map(({ href, label }) => (
-          <Link
+          <ScrollLink
             key={label}
-            className={`text-gray-500 hover:text-gray-600 ${
-              label == isSelected ? 'text-gray-600 font-bold' : ''
+            to={href}
+            smooth={true}
+            duration={500}
+            spy={true}
+            offset={-70}
+            className={`text-gray-500 hover:text-gray-600 cursor-pointer ${
+              label === isSelected ? 'text-primary font-bold' : ''
             }`}
-            href={href}
-            onClick={() => setIsSelected(label)}
+            onSetActive={() => setIsSelected(label)}
           >
             {label}
-          </Link>
+          </ScrollLink>
         ))}
       </div>
     </nav>
