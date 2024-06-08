c03509a8dcf87eaabecaf0379aa6ce34051b7452
Yuval Shalom <yuvalysh0@gmail.com>
2024-06-08 17:10:09 +0300

    Fixed color in the hero seciton

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import { heroText } from '../utils/heroText';
-import { playfair } from '../utils/fonts';
+import React from "react";
+import { heroText } from "../utils/heroText";
+import { playfair } from "../utils/fonts";
 
 const Hero = () => {
   return (
     <div
-      className="flex flex-col md:justify-center min-h-[50vh] md:min-h-screen p-10 md:pe-56 bg-primary md:bg-inherit  text-white md:text-black"
-      id="home"
-    >
+      className="flex flex-col md:justify-center min-h-[50vh] md:min-h-screen p-10 md:pe-56 bg-primary md:bg-inherit text-white md:text-inherit text-center md:text-left"
+      id="home">
       <h1
-        className={`text-5xl md:text-8xl font-bold mb-2 ${playfair.className}`}
-      >
+        className={`text-5xl md:text-8xl font-bold mb-2 ${playfair.className}`}>
         {heroText.title}
       </h1>
       <h2 className="text-xl md:text-2xl mb-4">{heroText.subtitle}</h2>
