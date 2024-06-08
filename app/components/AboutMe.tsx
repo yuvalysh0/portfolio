eb8a3e0607821d09a44e4416bbd4767dd44cc32f
Yuval Shalom <yuvalysh0@gmail.com>
2024-06-08 17:10:19 +0300

    Refactor AboutMe component and fix formatting

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import { aboutMe } from '../utils/aboutMe';
-import { playfair } from '../utils/fonts';
-import { techStack } from '../utils/techStack';
-import TechIcon from './TechIcon';
-import { IconName } from '@fortawesome/fontawesome-svg-core';
+import React from "react";
+import { aboutMe } from "../utils/aboutMe";
+import { playfair } from "../utils/fonts";
+import { techStack } from "../utils/techStack";
+import TechIcon from "./TechIcon";
+import { IconName } from "@fortawesome/fontawesome-svg-core";
 
 const AboutMe = () => {
   const iconStyle =
-    'transform hover:scale-110 transition-transform w-10 h-10 hover:text-primary';
+    "transform hover:scale-110 transition-transform w-10 h-10 hover:text-primary";
   return (
     <div
       className="flex flex-col justify-center min-h-[70vh] md:min-h-screen p-10 md:pe-56"
-      id="about"
-    >
+      id="about">
       <h1
-        className={`text-4xl md:text-6xl font-bold mb-2 ${playfair.className}`}
-      >
+        className={`text-4xl md:text-6xl font-bold mb-2 ${playfair.className}`}>
         About Me.
       </h1>
       <p className="text-lg md:text-xl font-light mb-4">
@@ -25,7 +23,7 @@ const AboutMe = () => {
       <h2 className={`text-lg md:text-xl font-medium mb-2`}>
         Technologies I've worked with:
       </h2>
-      <div className="flex space-x-4">
+      <div className="grid grid-cols-4 md:flex md:flex-wrap gap-4">
         {techStack.map(({ name, iconName }) => (
           <TechIcon
             key={name}
