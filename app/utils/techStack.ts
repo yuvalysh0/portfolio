4b2d857bac74db94ad274ebf3bb02e46037391a2
Yuval Shalom <yuvalysh0@gmail.com>
2024-06-08 17:10:25 +0300

    Refactor TechIcon component and update techStack

--- app/utils/techStack.ts
+++ app/utils/techStack.ts
@@ -1,26 +1,38 @@
 export const techStack = [
   {
-    iconName: 'html5',
-    name: 'HTML',
+    iconName: "html5",
+    name: "HTML",
   },
   {
-    iconName: 'css3-alt',
-    name: 'CSS',
+    iconName: "css3-alt",
+    name: "CSS",
   },
   {
-    iconName: 'js-square',
-    name: 'Javascript',
+    iconName: "js-square",
+    name: "Javascript",
   },
   {
-    iconName: 'react',
-    name: 'React',
+    iconName: "react",
+    name: "React",
   },
   {
-    iconName: 'vuejs',
-    name: 'Vuejs',
+    iconName: "vuejs",
+    name: "Vuejs",
   },
   {
-    iconName: 'node-js',
-    name: 'Nodejs',
+    iconName: "node-js",
+    name: "Nodejs",
+  },
+  {
+    iconName: "unity",
+    name: "Unity",
+  },
+  {
+    iconName: "git-alt",
+    name: "Git",
+  },
+  {
+    iconName: "npm",
+    name: "NPM",
   },
 ];
