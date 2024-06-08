796bb8a94b287e93caf45babc4179c268806e972
Yuval Shalom <yuvalysh0@gmail.com>
2024-06-08 17:09:43 +0300

    made social links on mobile

--- app/components/layout/ImageSection.tsx
+++ app/components/layout/ImageSection.tsx
@@ -1,43 +1,19 @@
-'use client';
-import Image from 'next/image';
-import React from 'react';
-import profilePic from '../../assets/images/profile-picture.jpg';
-import { SocialIcon } from '../SocialIcon';
-import { Toaster, toast } from 'react-hot-toast';
-import { IconName } from '@fortawesome/fontawesome-svg-core';
-
-export const socialItems = [
-  {
-    link: 'https://www.linkedin.com/in/yuvalshalom',
-    icon: 'linkedin-in',
-  },
-  { link: 'https://twitter.com/yuvalysh0', icon: 'twitter' },
-  { link: 'https://github.com/yuvalysh0', icon: 'github' },
-];
+"use client";
+import Image from "next/image";
+import React from "react";
+import profilePic from "../../assets/images/profile-picture.jpg";
+import { SocialIcon } from "../SocialIcon";
+import { Toaster, toast } from "react-hot-toast";
+import { IconName } from "@fortawesome/fontawesome-svg-core";
+import { socialItems } from "@/app/utils/socialItems";
 
 const ImageSection = () => {
   const iconsStyle =
-    'text-white text-xl md:text-2xl lg:text-3xl transform transition-transform duration-300 hover:scale-125 hover:rotate-12 md:hover:-translate-x-2';
+    "text-white text-xl md:text-2xl lg:text-3xl transform transition-transform duration-300 md:hover:scale-125 md:hover:rotate-12 md:hover:-translate-x-2";
 
   return (
-    <section className="relative w-screen h-[50vh] flex md:fixed md:right-0 md:top-0 md:h-full items-center justify-center bg-primary md:w-5/12">
-      <Image
-        onClick={() =>
-          toast(`I Know I'm cute but don't push me`, {
-            icon: '🤪',
-            duration: 2500,
-            style: {
-              border: '1px solid #65C3C7',
-              padding: '12px',
-              color: '#65C3C7',
-            },
-          })
-        }
-        className="md:sticky object-cover rounded-full overflow-hidden w-[65%] aspect-square md:w-56 lg:w-80 drop-shadow-2xl shadow-slate-400 md:hover:border-2 md:hover:border-sky-700 md:transition-all md:duration-200 md:hover:scale-105"
-        src={profilePic}
-        alt="Profile Picture"
-      />
-      <div className="hidden md:flex fixed right-0 top-1/2 transform -translate-y-1/2 flex-col space-y-4 pr-4">
+    <section className="relative w-screen h-[50vh] flex flex-col md:flex-row md:fixed md:right-0 md:top-0 md:h-full items-center justify-center bg-primary md:w-5/12">
+      <div className="flex md:flex-col gap-4 mb-8 md:fixed md:right-0 md:top-1/2 transform md:-translate-y-1/2 md:pr-4">
         {socialItems.map(({ link, icon }) => (
           <SocialIcon
             key={link}
@@ -47,6 +23,22 @@ const ImageSection = () => {
           />
         ))}
       </div>
+      <Image
+        onClick={() =>
+          toast(`I Know I'm cute but don't push me`, {
+            icon: "🤪",
+            duration: 2500,
+            style: {
+              border: "1px solid #65C3C7",
+              padding: "12px",
+              color: "#65C3C7",
+            },
+          })
+        }
+        className="md:sticky object-cover rounded-full overflow-hidden w-[65%] aspect-square md:w-56 lg:w-80 drop-shadow-2xl shadow-slate-400 md:hover:border-2 md:hover:border-sky-700 md:transition-all md:duration-200 md:hover:scale-105"
+        src={profilePic}
+        alt="Profile Picture"
+      />
       <Toaster />
     </section>
   );
---
+++ app/utils/socialItems.ts
@@ -0,0 +1,8 @@
+export const socialItems = [
+  {
+    link: "https://www.linkedin.com/in/yuvalshalom",
+    icon: "linkedin-in",
+  },
+  { link: "https://twitter.com/yuvalysh0", icon: "twitter" },
+  { link: "https://github.com/yuvalysh0", icon: "github" },
+];
