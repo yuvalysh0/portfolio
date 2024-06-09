b3b6b7f74800baad075913a29bf44ecd7036aa71
Yuval Shalom <yuvalysh0@gmail.com>
2024-06-09 09:34:23 +0300

    Update Hero and ImageSection styles and components

--- app/components/layout/ImageSection.tsx
+++ app/components/layout/ImageSection.tsx
@@ -1,16 +1,16 @@
-"use client";
-import Image from "next/image";
-import React from "react";
-import profilePic from "../../assets/images/profile-picture.jpg";
-import { SocialIcon } from "../SocialIcon";
-import { Toaster, toast } from "react-hot-toast";
-import { IconName } from "@fortawesome/fontawesome-svg-core";
-import { socialItems } from "@/app/utils/socialItems";
-import AnimatedSection from "./AnimatedSection";
+'use client';
+import Image from 'next/image';
+import React from 'react';
+import profilePic from '../../assets/images/profile-picture.jpg';
+import { SocialIcon } from '../SocialIcon';
+import { Toaster, toast } from 'react-hot-toast';
+import { IconName } from '@fortawesome/fontawesome-svg-core';
+import { socialItems } from '@/app/utils/socialItems';
+import AnimatedSection from './AnimatedSection';
 
 const ImageSection = () => {
   const iconsStyle =
-    "text-white text-2xl lg:text-3xl transform transition-transform duration-300 md:hover:scale-125 md:hover:rotate-12 md:hover:-translate-x-2";
+    'text-white text-4xl md:text-3xl transform transition-transform duration-300 md:hover:scale-125 md:hover:rotate-12 md:hover:-translate-x-2';
 
   return (
     <section className="relative w-screen h-[50vh] flex flex-col md:flex-row md:fixed md:right-0 md:top-0 md:h-full items-center justify-center bg-primary md:w-5/12">
@@ -28,16 +28,16 @@ const ImageSection = () => {
         <Image
           onClick={() =>
             toast(`I Know I'm cute but don't push me`, {
-              icon: "🤪",
+              icon: '🤪',
               duration: 2500,
               style: {
-                border: "1px solid #65C3C7",
-                padding: "12px",
-                color: "#65C3C7",
+                border: '1px solid #65C3C7',
+                padding: '12px',
+                color: '#65C3C7',
               },
             })
           }
-          className="md:sticky object-cover rounded-full overflow-hidden w-[65%] aspect-square md:w-56 lg:w-80 drop-shadow-2xl shadow-slate-400 md:hover:border-2 md:hover:border-sky-700 md:transition-all md:duration-200 md:hover:scale-105"
+          className="md:sticky object-cover rounded-full overflow-hidden w-[65%] aspect-square md:w-56 lg:w-80 drop-shadow-2xl shadow-slate-400 md:hover:border-2 md:hover:border-sky-700 md:transition-all md:duration-200 md:hover:scale-105 z-10"
           src={profilePic}
           alt="Profile Picture"
         />
