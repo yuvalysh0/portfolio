00adeb0e944ed52e4a1293f44357d0acbce5c362
yuvalysh0 <yuvalysh0@gmail.com>
2024-09-18 12:53:41 +0300

    Refactor layout component and update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { poppins } from "../utils/fonts";
-import NavBar from "./components/layout/NavBar";
-import ImageSection from "./components/layout/ImageSection";
 import LayoutWrapper from "./components/layout/LayoutWrapper";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Yuval Shalom's Portfolio",
@@ -16,14 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
+    <html>
+      <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <title>{metadata.title}</title>
+        <title>"Yuval Shalom's Portfolio"</title>
         <meta name="description" content={metadata.description || ""} />
-      </head>
+      </Head>
       <body className={poppins.className}>
         <main className="fle flex-col md:flex-row relative">
           <section className="md:w-7/12 flex flex-col">
